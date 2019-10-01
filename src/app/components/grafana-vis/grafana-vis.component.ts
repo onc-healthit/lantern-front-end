eae7a73a8418275a0414179f4f1fd200b2b18f34
Brianna Mathiowetz <bmathiowetz@mitre.org>
2019-10-01 16:17:47 -0400

    Add a log message to the grafana component and update test files.

--- src/app/components/grafana-vis/grafana-vis.component.ts
+++ src/app/components/grafana-vis/grafana-vis.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-grafana-vis',
@@ -11,10 +12,11 @@ export class GrafanaVisComponent implements OnInit {
   @Input() inputUrl: string;
   public grafanaUrl: SafeResourceUrl;
 
-  constructor(private sanitizer: DomSanitizer) {   }
+  constructor(private sanitizer: DomSanitizer, private logger: NGXLogger) {   }
 
   ngOnInit() {
     this.grafanaUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.inputUrl);
+    this.logger.info('Created Grafana Visualization from url: ' + this.inputUrl);
   }
 
 }
