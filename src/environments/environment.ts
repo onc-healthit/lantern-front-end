f0133dede6222349b0072f1504c1f574631456dd
Brianna Mathiowetz <bmathiowetz@mitre.org>
2019-10-01 16:16:53 -0400

    Set up logging using the ngx-logger library.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,18 @@
+import { NgxLoggerLevel } from 'ngx-logger';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:4200/',
+  // Display all logs in the console in development mode
+  logLevel: NgxLoggerLevel.TRACE,
+  // Do not use server-side logging in development mode
+  serverLogLevel: NgxLoggerLevel.OFF,
+  // Expose source maps in development mode
+  enableSourceMaps: true
 };
 
 /*
