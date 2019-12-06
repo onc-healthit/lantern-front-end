bb588350af4e4f127c6a3d44353f11bcbe4758ed
Matt Mayer <mmayer@mitre.org>
2019-12-06 14:47:21 -0500

    Use test

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-until ls /usr/local/share/ca-certificates/ca-certificates.crt
-do
-    echo "Waiting for /usr/local/share/ca-certificates/ca-certificates.crt to be available"
-    echo "Did you mount a volume to this location containing your root certificates?"
-done
-update-ca-certificates
-yarn config set cafile /usr/local/share/ca-certificates/ca-certificates.crt
+# Set custom ca-cert if it was passed in via volume
+if test -f /usr/local/share/ca-certificates/ca-certificates.crt then
+    update-ca-certificates
+    yarn config set cafile /usr/local/share/ca-certificates/ca-certificates.crt
+fi
 
 yarn install
 yarn global add @angular/cli@8.3.12
