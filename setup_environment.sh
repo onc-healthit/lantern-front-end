e1ef36c18a52b9cb9d39fcaeb97d13b9b7d61d9f
Matt Mayer <mmayer@mitre.org>
2019-12-06 15:14:49 -0500

    Try with wait loop again

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Set custom ca-cert if it was passed in via volume
-if test -f /usr/local/share/ca-certificates/ca-certificates.crt then
-    update-ca-certificates
-    yarn config set cafile /usr/local/share/ca-certificates/ca-certificates.crt
-fi
+until ls /usr/local/share/ca-certificates/ca-certificates.crt
+do
+    echo "Waiting for /usr/local/share/ca-certificates/ca-certificates.crt to be available"
+    echo "Did you mount a volume to this location containing your root certificates?"
+done
+update-ca-certificates
+yarn config set cafile /usr/local/share/ca-certificates/ca-certificates.crt
 
 yarn install
 yarn global add @angular/cli@8.3.12
